9c0290019dc96c6e3e77ea3e40fbb84de4ce4399
Ramil <89657343+kamalovvvv@users.noreply.github.com>
2022-09-09 15:34:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //https://api.telegram.org/bot5139970119:AAFILbesf38UHeI5f9KFPxoicO3WimdnsmE/getUpdates
 
 function BotSendForm() {
-    let message = document.querySelector('.message').value;
-    const token = '5139970119:AAFILbesf38UHeI5f9KFPxoicO3WimdnsmE';
-    let url = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=818834347&text=';
-    //let url = 'https://api.telegram.org/bot5139970119:AAFILbesf38UHeI5f9KFPxoicO3WimdnsmE/sendMessage?chat_id=818834347&text='
-    let xhttp = new XMLHttpRequest();
-    xhttp.open("GET", url + message, true);
-    xhttp.send();
+  let message = document.querySelector('.message').value;
+  const token = '5139970119:AAFILbesf38UHeI5f9KFPxoicO3WimdnsmE';
+  let url = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=818834347&text=';
+  //let url = 'https://api.telegram.org/bot5139970119:AAFILbesf38UHeI5f9KFPxoicO3WimdnsmE/sendMessage?chat_id=818834347&text='
+  let xhttp = new XMLHttpRequest();
+  xhttp.open("GET", url + message, true);
+  xhttp.send();
 }
