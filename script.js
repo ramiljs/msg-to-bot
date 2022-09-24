609006553a9db19d2f6da91fa725ce59defd5235
Ramil <89657343+kamalovvvv@users.noreply.github.com>
2022-09-24 22:17:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-//https://api.telegram.org/bot5139970119:AAFILbesf38UHeI5f9KFPxoicO3WimdnsmE/getUpdates
-
 function BotSendForm() {
   let message = document.querySelector('.message').value;
-  const token = '5139970119:AAFILbesf38UHeI5f9KFPxoicO3WimdnsmE';
+  const token = '55139970119:AAFILbesf38UHeI5f9KFPxoicO3WimdnsmE';
   let url = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=818834347&text=';
   let xhttp = new XMLHttpRequest();
   xhttp.open("GET", url + message, true);
